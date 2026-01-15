bf6a5a5f2716ff432c62dee79b23ba925900f349
Rajiv Mayani <mayani@isi.edu>
2026-01-14 16:03:40 -0800

    feat: numbers for 10 runs of setting up a 5, 10, and 15 node HTCondor cluster

--- collect-runtimes.sh
+++ collect-runtimes.sh
@@ -2,13 +2,20 @@
 
 set -e
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <experiment-dir> <count>"
+if [ $# -gt 3 ]; then
+    echo "Usage: $0 <experiment-dir> [start] <count>"
     exit 1
 fi
 
 EXPERIMENT_DIR=$1
-COUNT=$2
+
+if [ $# -eq 2 ]; then
+    START=1
+    COUNT=$2
+else
+    START=$2
+    COUNT=$3
+fi
 
 echo "Experiment Directory: $EXPERIMENT_DIR"
 echo "Count: $COUNT"
@@ -16,13 +23,15 @@ echo "Count: $COUNT"
 pushd $EXPERIMENT_DIR > /dev/null
 kiso check
 
-for i in $(seq 1 $COUNT); do
-    echo "Running experiment $i / $COUNT"
+for i in $(seq $START $COUNT); do
+    echo "- Running experiment $i / $COUNT -"
     mkdir output-$i
-    time kiso --debug up --output ./output-$i > ./output-$i/kiso.log 2>&1
+    time kiso --debug up --output ./output-$i
+
+    time kiso --debug run --output ./output-$i
 
     kiso --debug down --output ./output-$i
-    echo "Finished running experiment $i / $COUNT"
+    echo "- Finished running experiment $i / $COUNT -"
 done
 
 popd > /dev/null
